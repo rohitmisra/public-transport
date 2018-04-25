2c27c803b0835e27a3df29b31455b2928c8d4196
Rohit Misra <rohitmisra.44@gmail.com>
2018-04-25 09:27:23 +0200

    fixing headers

--- index.js
+++ index.js
@@ -150,6 +150,10 @@ var deptINOptions = function(stationObj) {
             stn: stationObj.id,
             stnqt: 'live'
         },
+	 headers: {
+            'Referer': 'http://etrain.info/in?STATION=URL',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+        },
         transform: deptINCallback
     };
 }
